41b520cc7f319f7177f8a396ee276dd42aacc3b1
syhanjin <syhanjin666@gmail.com>
2025-11-08 17:55:25 +0800

    test: add test for chassis_mecanum4 + chassis_if

--- UserCode/app/app.c
+++ UserCode/app/app.c
@@ -9,19 +9,32 @@
 #include "can.h"
 #include "cmsis_os2.h"
 #include "tim.h"
-#include "controllers/chassis_mecanum4.h"
+#include "usart.h"
+#include "../drivers/chassis_mecanum4.h"
 #include "drivers/DJI.h"
 #include "interfaces/motor_if.h"
+#include "drivers/HWT101CT.h"
+#include "interfaces/chassis_if.h"
 
 DJI_t           wheel_dji[4];
 Motor_VelCtrl_t motor_vel_ctrl[4];
-Mecanum4_t      chassis_mecanum4;
+Chassis_t       chassis;
+HWT101CT_t      sensor;
 
 void CtrlTIM_Callback(TIM_HandleTypeDef* htim)
 {
-    Mecanum4_ControlUpdate(&chassis_mecanum4);
+    // Mecanum4_Update(&chassis_mecanum4);
+    Chassis_Update(&chassis);
 
     DJI_SendSetIqCommand(&hcan1, IQ_CMD_GROUP_1_4);
+}
+
+void Uart_RxCpltCallback(UART_HandleTypeDef* huart)
+{
+    if (huart == sensor.huart)
+    {
+        HWT101CT_RxCallback(&sensor);
+    }
 }
 
 /**
@@ -32,6 +45,10 @@ void CtrlTIM_Callback(TIM_HandleTypeDef* htim)
 void Init(void* argument)
 {
     /* 初始化代码 */
+
+    HAL_UART_RegisterCallback(&huart3, HAL_UART_RX_COMPLETE_CB_ID, Uart_RxCpltCallback);
+    HWT101CT_Init(&sensor, &huart3);
+    HWT101CT_ResetYaw(&sensor);
 
     // 初始化 CAN
     DJI_CAN_FilterInit(&hcan1, 0);
@@ -81,9 +98,8 @@ void Init(void* argument)
                                                                      .Kd             = 5.00f,
                                                                      .abs_output_max = 8000.0f } });
 
-    Mecanum4_Init(&chassis_mecanum4,
-                  (Mecanum4_Config_t) {
-                          .heading_lock      = true,               ///< 设置速度后是否锁定航向
+    Chassis_Init(&chassis, &(Chassis_Config_t) {
+        .driver = {
                           .chassis_type      = MECANUM4_O_TYPE,    ///< 底盘构型
                           .wheel_radius      = 77.0f,              ///< 轮子半径 (unit: mm)
                           .wheel_distance_x  = 504.60f,            ///< 左右轮距 (unit: mm)
@@ -92,12 +108,17 @@ void Init(void* argument)
                           .wheel_front_left  = &motor_vel_ctrl[1], ///< 左前方
                           .wheel_rear_left   = &motor_vel_ctrl[2], ///< 左后方
                           .wheel_rear_right  = &motor_vel_ctrl[3], ///< 右后方
-                  });
+                  },
+        .feedback_source = {
+            .yaw = &sensor.yaw,
+        },
+    });
 
     HAL_TIM_RegisterCallback(&htim6, HAL_TIM_PERIOD_ELAPSED_CB_ID, CtrlTIM_Callback);
     HAL_TIM_Base_Start_IT(&htim6);
 
     osDelay(3000);
+    Chassis_SetWorldFromCurrent(&chassis);
     // Mecanum4_SetVelocity(&chassis_mecanum4,
     //                      (Mecanum4_Velocity_t) {
     //                              .vx    = 0.5f,
@@ -112,13 +133,49 @@ void Init(void* argument)
     //                              .omega = 0.0f,
     //                      });
     // osDelay(5000);
-    Mecanum4_SetVelocity(&chassis_mecanum4,
-                         (Mecanum4_Velocity_t) {
-                                 .vx    = 0.25f,
-                                 .vy    = 0.0f,
-                                 .omega = 45.0f,
-                         });
-    osDelay(8000);
+
+    // Chassis_SetVelBodyFrame(&chassis,
+    //                         &(Chassis_Velocity_t) {
+    //                                 .vx = 0.2f,
+    //                                 .vy = 0.0f,
+    //                                 .wz = 0.0f,
+    //                         });
+    // osDelay(1500);
+    // Chassis_SetVelBodyFrame(&chassis,
+    //                         &(Chassis_Velocity_t) {
+    //                                 .vx = 0.0f,
+    //                                 .vy = 0.2f,
+    //                                 .wz = 0.0f,
+    //                         });
+    // osDelay(1500);
+    // Chassis_SetVelBodyFrame(&chassis,
+    //                         &(Chassis_Velocity_t) {
+    //                                 .vx = 0.0f,
+    //                                 .vy = 0.0f,
+    //                                 .wz = 30.0f,
+    //                         });
+    // osDelay(1500);
+
+    Chassis_SetTargetPostureInWorld(&chassis, &(Chassis_PostureTarget_t) {
+        .posture = {
+            .x   = 2.0f,
+            .y   = 0.0f,
+            .yaw = 180.0f,
+        },
+        .speed  = 0.25f,
+        .omega  = 30.0f,
+    });
+    // osDelay(8000);
+    // Chassis_SetTargetPostureInWorld(&chassis, &(Chassis_PostureTarget_t) {
+    //     .posture = {
+    //         .x   = 0.0f,
+    //         .y   = 0.0f,
+    //         .yaw = 0.0f,
+    //     },
+    //     .speed  = 0.5f,
+    //     .omega  = 45.0f,
+    // });
+    // osDelay(8000);
     // Mecanum4_SetVelocity(&chassis_mecanum4,
     //                      (Mecanum4_Velocity_t) {
     //                              .vx    = -0.1f,
@@ -126,8 +183,8 @@ void Init(void* argument)
     //                              .omega = -10.0f,
     //                      });
     // osDelay(30000);
-    Mecanum4_SetVelocity(&chassis_mecanum4,
-                         (Mecanum4_Velocity_t) { .vx = 0.0f, .vy = 0.0f, .omega = 0.0f });
+    // Chassis_SetVelBodyFrame(&chassis, &(Chassis_Velocity_t) { .vx = 0.0f, .vy = 0.0f, .wz = 0.0f
+    // });
 
     /* 初始化完成后退出线程 */
     osThreadExit();
